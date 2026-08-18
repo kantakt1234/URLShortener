21bdff4de0faf6797b4213ee2801c9d3f3f85f7e
Никита <nik.cher.chernov@gmail.com>
2026-08-18 03:11:22 +0300

    create class URLIn and update class URLOut

--- app/urls/schemas.py
+++ app/urls/schemas.py
@@ -1,8 +1,13 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, AnyUrl
 
 
-class URLOut(BaseModel):
-    token: str
-    original_url: str
+class URLIn(BaseModel):
+    original_url: AnyUrl
 
     model_config = ConfigDict(from_attributes=True)
+
+
+class URLOut(URLIn):
+    id: int
+    token: str
+    clicks: int
